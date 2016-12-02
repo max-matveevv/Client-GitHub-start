6b0c58253b0a3f9b2e43f8dffbb84dacf72e840d
max-matveev <max.matveevv@gmail.com>
2016-12-02 21:48:05 +0600

    Repository list

---
+++ Client GitHub/InformationRepository.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  Client GitHub
+//
+//  Created by Max on 28.11.16.
+//  Copyright © 2016 Max. All rights reserved.
+//
+
+import Foundation
+
+
+class InformationRepository: UIViewController {
+    
+}
