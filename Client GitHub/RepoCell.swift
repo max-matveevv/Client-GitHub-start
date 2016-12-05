e7022fc1948dc6025f8801f0240972b9838b786f
max-matveev <max.matveevv@gmail.com>
2016-12-06 00:41:19 +0600

    RepoCell, fixed layout list

---
+++ Client GitHub/RepoCell.swift
@@ -0,0 +1,31 @@
+//
+//  RepoCell.swift
+//  Client GitHub
+//
+//  Created by Max on 06.12.16.
+//  Copyright © 2016 Max. All rights reserved.
+//
+
+import UIKit
+
+class RepoCell: UITableViewCell {
+    
+    // fixed code
+/*(UITableViewCell *)tableView: (UITableView *)tableView
+    cellForRowAtIndexPath: (NSIndexPath *)indexPath {
+        static NSString *cellIdentifier = @"myCell";
+        MyCell *cell = (MyCell *) [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
+        if (cell == nil) {
+            NSArray *t = [[NSBundle mainBundle] loadNibNamed:@"myCellXibFileName"
+                                                    owner:nil options:nil];
+            for (id currentObject in t) {
+                if ([currentObject isKindOfClass:[MyCell class]]) {
+                    cell = (MyCell *)currentObject;
+                    break;
+                }
+            }
+        }
+    return cell;
+    }*/
+}
+ 
