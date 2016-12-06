073671440239c482fb04e202a08c7e9a97108ea3
kdavydov <kdavydov21@gmail.com>
2016-12-06 22:44:22 +0600

    CommitList

---
+++ Client GitHub/CommitList.swift
@@ -0,0 +1,22 @@
+//
+//  CommitList.swift
+//  Client GitHub
+//
+//  Created by Konstantin on 06.12.16.
+//  Copyright © 2016 Max. All rights reserved.
+//
+
+import Foundation
+
+class CommitList: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+
+    }    
+}
